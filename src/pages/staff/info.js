59bafc180753704cf3da2174e9f8f9c38a8abb3c
sun00108 <h243sun@uwaterloo.ca>
2022-10-29 17:53:37 -0500

    details in StaffInfo

--- src/pages/staff/info.js
+++ src/pages/staff/info.js
@@ -1,5 +1,6 @@
-import {Card, Col, Layout, Nav, Row, Button, Table} from "@douyinfe/semi-ui";
+import {Card, Col, Layout, Nav, Row, Button, Table, Image } from "@douyinfe/semi-ui";
 import { Carousel, Typography, Space } from '@douyinfe/semi-ui';
+import { IconUploadError } from '@douyinfe/semi-icons';
 import { Divider } from '@douyinfe/semi-ui';
 import React from "react";
 import AppHeader from "../../components/header";
@@ -64,13 +65,15 @@ export default function StaffInfo() {
     const [ data, setData ] = React.useState([]);
 
     const [ staffName, setStaffName ] = React.useState("");
+    const [ staffNameCN, setStaffNameCN ] = React.useState("");
 
     const fetchStaffInfo = () => {
         axios.get(process.env.REACT_APP_API_HOST + '/api/v1/relationships?staff_id=' + id).then( res => {
             setData(res.data.relationships)
         })
-        axios.get(process.env.REACT_APP_API_HOST + '/api/v1/staffs/' + id + '/name' ).then( res => {
-            setStaffName(res.data.name)
+        axios.get(process.env.REACT_APP_API_HOST + '/api/v1/staffs/' + id ).then( res => {
+            setStaffName(res.data.staff.name)
+            setStaffNameCN(res.data.staff.name_cn)
         })
     }
 
@@ -85,19 +88,26 @@ export default function StaffInfo() {
                 <div className="grid">
                     <Row type={"flex"} justify={"center"}>
                         <Col xs={24} xxl={12}>
-                            <Title style={{ margin: '8px 0' }} >STAFF 数据 - {staffName}</Title>
-                        </Col>
-                    </Row>
-                    <Row type={"flex"} justify={"center"}>
-                        <Col xs={24} xxl={12}>
-                            <Link to={"/staff"} style={{ textDecoration: 'none'}}>
-                                <Button type="tertiary" block>返回列表</Button>
-                            </Link>
-                        </Col>
-                    </Row>
-                    <Row type={"flex"} justify={"center"}>
-                        <Col xs={24} xxl={12}>
-                            <Table columns={columns} dataSource={data} pagination={false}/>
+                            <Row type={"flex"} justify={"center"}>
+                                <Col xs={{ span: 24, order: 2 }} lg={{ span: 16, order: 1 }}>
+                                    <Title style={{ margin: '8px 0' }} >{staffName}</Title>
+                                    <Text>中文名： {staffNameCN}</Text>
+                                    <br />
+                                    <Link to={"/staff"} style={{ textDecoration: 'none'}}>
+                                        <Button>返回列表</Button>
+                                    </Link>
+                                    <br />
+                                    <Table columns={columns} dataSource={data} pagination={false}/>
+                                </Col>
+                                <Col xs={{ span: 24, order: 1 }} lg={{ span: 4, order: 2 }}>
+                                    <Image
+                                        width={200}
+                                        height={200}
+                                        src="https://load-error.jpeg"
+                                        fallback={<IconUploadError style={{ fontSize: 50 }} />}
+                                    />
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
                 </div>
