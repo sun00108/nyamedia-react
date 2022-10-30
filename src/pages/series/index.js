b0374eaeeb12432c079fb5910099313f531e30d7
sun00108 <h243sun@uwaterloo.ca>
2022-10-30 03:18:13 -0500

    seriesIndex

--- src/pages/series/index.js
+++ src/pages/series/index.js
@@ -4,12 +4,25 @@ import { Divider } from '@douyinfe/semi-ui';
 import React from "react";
 import AppHeader from "../../components/header";
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 export default function SeriesIndex() {
 
     const { Title, Paragraph, Text } = Typography;
     const { Meta } = Card;
     const { Header, Footer, Content } = Layout;
+
+    const [ seriesList, setSeriesList ] = React.useState([]);
+
+    const fetchAllSeries = () => {
+        axios.get( process.env.REACT_APP_API_HOST + '/api/v1/series').then( res => {
+            setSeriesList(res.data.series)
+        })
+    }
+
+    React.useEffect(() => {
+        fetchAllSeries()
+    }, [])
 
     return (
         <Layout className="components-layout-demo">
@@ -20,6 +33,25 @@ export default function SeriesIndex() {
                         <Col xs={24} xxl={12}>
                             <Title style={{ margin: '8px 0' }} >剧集列表 - 施工中</Title>
                         </Col>
+                    </Row>
+                    <Row gutter={16} type={"flex"} justify={"center"}>
+                        {
+                            seriesList.map((item) => {
+                                return (
+                                    <Col xs={12} lg={8} xxl={2}>
+                                        <Link to={"/series/" + item.id} style={{ textDecoration: 'none'}}>
+                                            <Card cover={<img src={ process.env.REACT_APP_MINIO_HOST + "/nyamedia/series/" + item.id + "/poster/" + item.poster + ".jpg"} width={300} />}
+                                                  bordered={false} shadows='hover'>
+                                                <Meta
+                                                    title={item.name}
+                                                    description={item.name_cn}
+                                                />{"第" + item.season + "季"}
+                                            </Card>
+                                        </Link>
+                                    </Col>
+                                )
+                            })
+                        }
                     </Row>
                 </div>
             </Content>
